e4126d152cf5a1cd667e5bea2309da980f6f4d45
Ivan <ivanas.kirsanov@gmail.com>
2020-03-15 18:00:23 +0200

    Refactor listeners

--- src/ui/DefaultGrid.java
+++ src/ui/DefaultGrid.java
@@ -11,10 +11,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public class DefaultGrid extends JFrame {
 
@@ -51,128 +49,46 @@ public class DefaultGrid extends JFrame {
 
 		JButton btn1 = new JButton("");
 		buttonGroup.add(btn1);
-		btn1.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[0][0] == 0) {
-				board[0][0] = playingSide;
-				turnLogic(board, playingSide, btn1);
-				movesCount++;
-				computerMove();
-			}
-		});
 		btn1.setBounds(15, 16, 103, 95);
 		contentPane.add(btn1);
 
 		JButton btn2 = new JButton("");
 		buttonGroup.add(btn2);
-		btn2.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[0][1] == 0) {
-				board[0][1] = playingSide;
-				turnLogic(board, playingSide, btn2);
-				movesCount++;
-				computerMove();
-			}
-		});
 		btn2.setBounds(15, 114, 103, 95);
 		contentPane.add(btn2);
 
 		JButton btn3 = new JButton("");
 		buttonGroup.add(btn3);
-		btn3.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[0][2] == 0) {
-				board[0][2] = playingSide;
-				turnLogic(board, playingSide, btn3);
-				movesCount++;
-				computerMove();
-			}
-		});
 		btn3.setBounds(15, 211, 103, 95);
 		contentPane.add(btn3);
 
 		JButton btn4 = new JButton("");
 		buttonGroup.add(btn4);
-		btn4.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[1][0] == 0) {
-				board[1][0] = playingSide;
-				turnLogic(board, playingSide, btn4);
-				movesCount++;
-				computerMove();
-			}
-		});
 		btn4.setBounds(120, 16, 103, 95);
 		contentPane.add(btn4);
 
 		JButton btn5 = new JButton("");
 		buttonGroup.add(btn5);
-		btn5.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[1][1] == 0) {
-				board[1][1] = playingSide;
-				turnLogic(board, playingSide, btn5);
-				movesCount++;
-				computerMove();
-			}
-		});
 		btn5.setBounds(120, 114, 103, 95);
 		contentPane.add(btn5);
 
 		JButton btn6 = new JButton("");
 		buttonGroup.add(btn6);
-		btn6.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[1][2] == 0) {
-				board[1][2] = playingSide;
-				turnLogic(board, playingSide, btn6);
-				movesCount++;
-				computerMove();
-			}
-		});
 		btn6.setBounds(120, 211, 103, 95);
 		contentPane.add(btn6);
 
 		JButton btn7 = new JButton("");
 		buttonGroup.add(btn7);
-		btn7.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[2][0] == 0) {
-				board[2][0] = playingSide;
-				turnLogic(board, playingSide, btn7);
-				movesCount++;
-				computerMove();
-			}
-		});
 		btn7.setBounds(225, 16, 103, 95);
 		contentPane.add(btn7);
 
 		JButton btn8 = new JButton("");
 		buttonGroup.add(btn8);
-		btn8.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[2][1] == 0) {
-				board[2][1] = playingSide;
-				turnLogic(board, playingSide, btn8);
-				movesCount++;
-				computerMove();
-			}
-		});
 		btn8.setBounds(225, 114, 103, 95);
 		contentPane.add(btn8);
 
 		JButton btn9 = new JButton("");
 		buttonGroup.add(btn9);
-		btn9.addActionListener(e -> {
-			checkWhichSidePlaysNow();
-			if (board[2][2] == 0) {
-				board[2][2] = playingSide;
-				turnLogic(board, playingSide, btn9);
-				movesCount++;
-				computerMove();
-			}
-		});
-
 		btn9.setBounds(225, 211, 103, 95);
 		contentPane.add(btn9);
 
@@ -216,55 +132,83 @@ public class DefaultGrid extends JFrame {
 		buttonsPositionsInBoard.put("20", btn7);
 		buttonsPositionsInBoard.put("21", btn8);
 		buttonsPositionsInBoard.put("22", btn9);
+
+		ActionListener pushListener = e -> {
+			JButton button = (JButton) e.getSource();
+			String pos = getKey(buttonsPositionsInBoard, button);
+			int x= Integer.valueOf(pos.substring(0,1));
+			int y= Integer.valueOf(pos.substring(1));
+			checkWhichSidePlaysNow();
+
+			if (board[x][y] == 0) {
+				board[x][y] = playingSide;
+				turnLogic(board, playingSide, button);
+				movesCount++;
+				computerMove();
+
+			}
+		};
+
+		buttonGroup.forEach(b->b.addActionListener(pushListener));
+
 	}
 
-	private void checkWhichSidePlaysNow() {
-		if (playerX) {
-			playingSide = 'x';
-		} else {
-			playingSide = 'o';
+		private void checkWhichSidePlaysNow() {
+				if (playerX) {
+					playingSide = 'x';
+				} else {
+					playingSide = 'o';
+				}
+			}
+
+			private void turnLogic(char[][] board, char playingSide, JButton btn) {
+				btn.setText(String.valueOf(playingSide));
+				btn.setFont(new Font("Arial", Font.PLAIN, 40));
+				String result = checker.whoIsTheWinner(board, playingSide);
+				playerX = !playerX;
+				victoryLabel.setText(result);
+				victoryDisablesButtons(result);
+			}
+
+			private void restartGame() {
+				board = new char[3][3];
+				playingSide = 0;
+				playerX = true;
+				victoryLabel.setText("");
+				buttonGroup.forEach(b -> {
+					b.setEnabled(true);
+					b.setText("");
+				});
+				buttonGroup_1.forEach(b -> {
+					b.setEnabled(true);
+					b.setSelected(false);
+				});
+				movesCount = 0;
+			}
+
+			private void victoryDisablesButtons(String result) {
+				if (result.contains("Winner")) {
+					buttonGroup.forEach(b -> b.setEnabled(false));
+				}
+			}
+
+			private void computerMove() {
+				if (!checker.whoIsTheWinner(board, playingSide).contains("Next turn")) {
+					return;
+				}
+				checkWhichSidePlaysNow();
+				Position position = computerMove.makeComputerMove(board, movesCount, playingSide);
+				JButton button = buttonsPositionsInBoard.get(position.createXY());
+				turnLogic(board, playingSide, button);
+				movesCount++;
+			}
+
+			private <K, V> K getKey(Map<K, V> map, V value) {
+				for (Map.Entry<K, V> entry : map.entrySet()) {
+					if (entry.getValue().equals(value)) {
+						return entry.getKey();
+					}
+				}
+				return null;
+			}
 		}
-	}
-
-	private void turnLogic(char[][] board, char playingSide, JButton btn) {
-		btn.setText(String.valueOf(playingSide));
-		btn.setFont(new Font("Arial", Font.PLAIN, 40));
-		String result = checker.whoIsTheWinner(board, playingSide);
-		playerX = !playerX;
-		victoryLabel.setText(result);
-		victoryDisablesButtons(result);
-	}
-
-	private void restartGame() {
-		board = new char[3][3];
-		playingSide = 0;
-		playerX = true;
-		victoryLabel.setText("");
-		buttonGroup.forEach(b -> {
-			b.setEnabled(true);
-			b.setText("");
-		});
-		buttonGroup_1.forEach(b -> {
-			b.setEnabled(true);
-			b.setSelected(false);
-		});
-		movesCount=0;
-	}
-
-	private void victoryDisablesButtons(String result) {
-		if (result.contains("Winner")) {
-			buttonGroup.forEach(b -> b.setEnabled(false));
-		}
-	}
-
-	private void computerMove() {
-		if(!checker.whoIsTheWinner(board, playingSide).contains("Next turn")){
-			return;
-		}
-		checkWhichSidePlaysNow();
-		Position position = computerMove.makeComputerMove(board, movesCount, playingSide);
-		JButton button = buttonsPositionsInBoard.get(position.createXY());
-		turnLogic(board, playingSide, button);
-		movesCount++;
-	}
-}
